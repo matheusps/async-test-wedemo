e220d74feb6b314e0c498001d064b13dff5df278
matheusps <cout.matheusps@gmail.com>
2020-06-03 17:14:21 -0300

    Add tests

--- src/components/__tests__/AsyncCounter.test.js
+++ src/components/__tests__/AsyncCounter.test.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import { cleanup } from '@testing-library/react'
+import { cleanup, render, fireEvent, waitForElement } from '@testing-library/react'
 
 import AsyncCounter from '../AsyncCounter'
 
 afterEach(cleanup)
 
-it('should ...', () => {
-  expect(<AsyncCounter />).toBeTruthy()
+it('should update counter after 0.5s', async () => {
+  const { getByTestId, getByText } = render(<AsyncCounter />)
+  fireEvent.click(getByTestId('btn-up'))
+  const counter = await waitForElement(() => getByText('1'))
+  expect(counter).toHaveTextContent('1')
 })
--- src/components/__tests__/DataFetcher.test.js
+++ src/components/__tests__/DataFetcher.test.js
@@ -1,9 +1,63 @@
 import React from 'react'
-import { cleanup } from '@testing-library/react'
+import { cleanup, render, fireEvent, waitForElement } from '@testing-library/react'
 import DataFetcher from '../DataFetcher'
+import axios from 'axios'
 
-afterEach(cleanup)
+afterEach(() => {
+  cleanup()
+  jest.resetAllMocks()
+})
 
-it('should ...', () => {
-  expect(<DataFetcher />).toBeTruthy()
+jest.mock('axios')
+
+it('should display empty state', () => {
+  const { getByTestId } = render(<DataFetcher />)
+  expect(getByTestId('empty')).toHaveTextContent('Empty State')
+})
+
+it('should display data', async () => {
+  const url = '/hello'
+  const { getByTestId } = render(<DataFetcher url={url} />)
+
+  axios.get.mockResolvedValueOnce({
+    data: 'hello world'
+  })
+
+  fireEvent.click(getByTestId('fetch-data'))
+  const data = await waitForElement(() => getByTestId('data'))
+
+  expect(axios.get).toHaveBeenCalledTimes(1)
+  expect(axios.get).toHaveBeenCalledWith(url)
+  expect(data).toHaveTextContent('hello world')
+})
+
+
+it('should display nested data', async () => {
+  const url = '/hello'
+  const { getByTestId } = render(<DataFetcher extractor="resolved.message" url={url} />)
+
+  axios.get.mockResolvedValueOnce({
+    data: { resolved: { message: 'hello world nested' } }
+  })
+
+  fireEvent.click(getByTestId('fetch-data'))
+  const data = await waitForElement(() => getByTestId('data'))
+
+  expect(data).toHaveTextContent('hello world nested')
+})
+
+it('should display data on custom render', async () => {
+  const url = '/hello'
+  const { getByTestId } = render(<DataFetcher extractor="resolved.message" url={url}>
+    {({ data }) => <div data-testid="custom">{data}</div>}
+  </DataFetcher>)
+
+  axios.get.mockResolvedValueOnce({
+    data: { resolved: { message: 'hello world nested' } }
+  })
+
+  fireEvent.click(getByTestId('fetch-data'))
+  const data = await waitForElement(() => getByTestId('custom'))
+
+  expect(data).toHaveTextContent('hello world nested')
 })
